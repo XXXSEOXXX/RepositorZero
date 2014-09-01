b06acd9c0f1b8f526d86610a6e2db8b4741fd34d
XXXSEOXXX <273923070@qq.com>
2014-09-01 17:32:06 +0800

    Test and Alter

--- AzureTest/Program.cs
+++ AzureTest/Program.cs
@@ -15,30 +15,37 @@ namespace AzureTest
 
             List<Computer> computers = new List<Computer>();
             List<Request> requests = new List<Request>();
-            List<Record> record = new List<Record>();
+            List<Record> records = new List<Record>();
 
-            Console.WriteLine("Plase enter the number of times a test:\n");
+            Console.WriteLine("Plase enter the number of times a test:");
             int.TryParse(Console.ReadLine(), out t);
             while (CheckT(t))
             {
-                Console.WriteLine("The input is not currect,plase try again .\n");
+                Console.WriteLine("The input is not currect,plase try again .");
                 int.TryParse(Console.ReadLine(), out t);
             }
             for (int i = 0; i < t; i++)
             {
-                Console.WriteLine("Plase enter the number of computers and the number of cores:\n");
+                Console.WriteLine("Plase enter the number of computer and the number of request:");
                 string str = Console.ReadLine();
-                m = Convert.ToInt16(str.Split(new char[] { ' ' }, 2)[0]);
-                q = Convert.ToInt16(str.Split(new char[] { ' ' }, 2)[1]);
-                while (CheckM(m) || CheckQ(q))
+                while (str.Split(new char[] { ' ' }).Count() != 2)
                 {
-                    Console.WriteLine("The input is not currect,plase try again .\n");
-                    str=Console.ReadLine();
-                    m = Convert.ToInt16(str.Split(new char[] { ' ' }, 2)[0]);
-                    q = Convert.ToInt16(str.Split(new char[] { ' ' }, 2)[1]);
+                    Console.WriteLine("The input is not currect,plase try again .");
+                    str = Console.ReadLine();
                 }
 
-                Console.WriteLine(string.Format("Plase enter number of cores for {0} computers", m.ToString()));
+                int.TryParse(str.Split(new char[] { ' ' })[0], out m);
+                int.TryParse(str.Split(new char[] { ' ' })[1], out q);
+
+                while (CheckM(m) || CheckQ(q))
+                {
+                    Console.WriteLine("The input is not currect,plase try again .");
+                    str=Console.ReadLine();
+                    int.TryParse(str.Split(new char[] { ' ' })[0], out m);
+                    int.TryParse(str.Split(new char[] { ' ' })[1], out q);
+                }
+
+                Console.WriteLine(string.Format("Plase enter number of core for {0} computer:", m.ToString()));
 
                 for (int mi = 0; mi < m; mi++)
                 {
@@ -52,7 +59,7 @@ namespace AzureTest
                     int.TryParse(Console.ReadLine(), out ci);
                     while (CheckCi(ci))
                     {
-                        Console.WriteLine("The input is not currect,plase try again .\n");
+                        Console.WriteLine("The input is not currect,plase try again .");
                         int.TryParse(Console.ReadLine(), out ci);
                     }
                     comp.core = ci;
@@ -61,7 +68,7 @@ namespace AzureTest
                     computers.Add(comp);
                 }
 
-                Console.WriteLine(string.Format("Plase enter {0} requests",q.ToString()));
+                Console.WriteLine(string.Format("Plase enter {0} requests:",q.ToString()));
 
                 for (int qi = 0; qi < q; qi++)
                 {
@@ -72,11 +79,19 @@ namespace AzureTest
                     string strReq = Console.ReadLine();
                     char reqType=' ';
                     int cq = 0;
+                    while (strReq.Split(new char[] { ' ' }).Count() != 2)
+                    {
+                        Console.WriteLine("The input is not currect,plase try again .");
+                        strReq = Console.ReadLine();
+                    }
+
                     char.TryParse(strReq.Split(new char[] { ' ' }, 2)[0], out reqType);
                     int.TryParse(strReq.Split(new char[] { ' ' }, 2)[1], out cq);
+
                     while (CheckQtype(reqType) || CheckCi(cq))
                     {
-                        Console.WriteLine("The input is not currect,plase try again .\n");
+                        Console.WriteLine("The input is not currect,plase try again .");
+                        strReq = Console.ReadLine();
                         char.TryParse(strReq.Split(new char[] { ' ' }, 2)[0], out reqType);
                         int.TryParse(strReq.Split(new char[] { ' ' }, 2)[1], out cq);
                     }
@@ -94,19 +109,66 @@ namespace AzureTest
                 for (int qi = 0; qi < q; qi++)
                 {
                     var rTemp = requests.Where(z => z.tID == ti && z.reqID == qi).FirstOrDefault();
-                    string str=Solution(rTemp);
+                    string str=Solution(rTemp,computers,records);
                 }
             }
+            Console.ReadLine();
+            return;
 
         }
 
-        private static string Solution(Request rTemp)
+        private static string Solution(Request rTemp,List<Computer> comList,List<Record> reList)
         {
-            if(rTemp.reqType=='A')
+            string restr = "";
+            if (rTemp.reqType == 'A')
             {
+                //int k = 0;
+                foreach (Computer cpt in comList)
+                {
+                    if (cpt.core - cpt.used < rTemp.reqNum || cpt.tID != rTemp.tID)
+                        continue;
 
+                    int rq = rTemp.reqNum;
+                    Record rec = new Record();
+                    rec.tID = cpt.tID;
+                    rec.reqID = rTemp.reqID;
+                    rec.compID = cpt.compID;
+                    rec.coreused = new int[rq];
+                    int ct = rec.coreused.Count();
+
+                    for (int co = 0; co < cpt.coreStatus.Count(); co++)
+                    {
+                        if (cpt.coreStatus[co] == 1)
+                            continue;
+                        if (rq <= 0)
+                            break;
+                        rec.coreused[ct - rq] = co;
+                        cpt.coreStatus[co] = 0;
+                        rq = rq - 1;
+                        cpt.used = cpt.used + 1;
+                    }
+                    reList. Add(rec);
+                    restr = string.Format("{0} {1}", rec.compID.ToString(), rec.coreused[0].ToString());
+                }
             }
-            return "";
+            else
+            {
+                var rec2 = reList.Where(z => z.tID == rTemp.tID && z.reqID == rTemp.reqID).FirstOrDefault();
+                if (rec2 != null)
+                {
+                    var tgcomp = comList.Where(tg => tg.tID == rec2.tID && tg.compID == rec2.compID).FirstOrDefault();
+                    foreach (int reused in rec2.coreused)
+                    {
+                        tgcomp.coreStatus[reused] = 0;
+                        tgcomp.used = tgcomp.used - 1;
+                    }
+                    restr = string.Format("{0} {1}",tgcomp.compID.ToString(),rec2.coreused[0].ToString());
+                }
+            }
+
+            if (restr == "")
+                restr = "-1 -1";
+            return restr;
         }
 
         private static bool CheckT(int input)
