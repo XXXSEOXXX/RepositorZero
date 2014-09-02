53e6706941c52c1aa6251a148c681affa5bd20f6
XXXSEOXXX <273923070@qq.com>
2014-09-02 17:32:59 +0800

    Azure Test Finish

--- AzureTest/Program.cs
+++ AzureTest/Program.cs
@@ -28,22 +28,21 @@ namespace AzureTest
             {
                 Console.WriteLine("Plase enter the number of computer and the number of request:");
                 string str = Console.ReadLine();
-                while (str.Split(new char[] { ' ' }).Count() != 2)
+                while (str.Split(new char[] { ' ' }).Count() != 2 || CheckM(str.Split(new char[] { ' ' })[0]) || CheckQ(str.Split(new char[] { ' ' })[1]))
                 {
                     Console.WriteLine("The input is not currect,plase try again .");
                     str = Console.ReadLine();
                 }
-
                 int.TryParse(str.Split(new char[] { ' ' })[0], out m);
                 int.TryParse(str.Split(new char[] { ' ' })[1], out q);
 
-                while (CheckM(m) || CheckQ(q))
-                {
-                    Console.WriteLine("The input is not currect,plase try again .");
-                    str=Console.ReadLine();
-                    int.TryParse(str.Split(new char[] { ' ' })[0], out m);
-                    int.TryParse(str.Split(new char[] { ' ' })[1], out q);
-                }
+                //while (CheckM(m) || CheckQ(q))
+                //{
+                //    Console.WriteLine("The input is not currect,plase try again .");
+                //    str=Console.ReadLine();
+                //    int.TryParse(str.Split(new char[] { ' ' })[0], out m);
+                //    int.TryParse(str.Split(new char[] { ' ' })[1], out q);
+                //}
 
                 Console.WriteLine(string.Format("Plase enter number of core for {0} computer:", m.ToString()));
 
@@ -79,7 +78,7 @@ namespace AzureTest
                     string strReq = Console.ReadLine();
                     char reqType=' ';
                     int cq = 0;
-                    while (strReq.Split(new char[] { ' ' }).Count() != 2)
+                    while (strReq.Split(new char[] { ' ' }).Count() != 2 || CheckQtype(strReq.Split(new char[] { ' ' }, 2)[0]) || CheckReqNum(strReq.Split(new char[] { ' ' }, 2)[1]))
                     {
                         Console.WriteLine("The input is not currect,plase try again .");
                         strReq = Console.ReadLine();
@@ -88,13 +87,13 @@ namespace AzureTest
                     char.TryParse(strReq.Split(new char[] { ' ' }, 2)[0], out reqType);
                     int.TryParse(strReq.Split(new char[] { ' ' }, 2)[1], out cq);
 
-                    while (CheckQtype(reqType) || CheckCi(cq))
-                    {
-                        Console.WriteLine("The input is not currect,plase try again .");
-                        strReq = Console.ReadLine();
-                        char.TryParse(strReq.Split(new char[] { ' ' }, 2)[0], out reqType);
-                        int.TryParse(strReq.Split(new char[] { ' ' }, 2)[1], out cq);
-                    }
+                    //while (CheckQtype(reqType) || CheckCi(cq))
+                    //{
+                    //    Console.WriteLine("The input is not currect,plase try again .");
+                    //    strReq = Console.ReadLine();
+                    //    char.TryParse(strReq.Split(new char[] { ' ' }, 2)[0], out reqType);
+                    //    int.TryParse(strReq.Split(new char[] { ' ' }, 2)[1], out cq);
+                    //}
                     req.reqType = reqType;
                     req.reqNum = cq;
 
@@ -110,6 +109,7 @@ namespace AzureTest
                 {
                     var rTemp = requests.Where(z => z.tID == ti && z.reqID == qi).FirstOrDefault();
                     string str=Solution(rTemp,computers,records);
+                    Console.WriteLine(str + "\n");
                 }
             }
             Console.ReadLine();
@@ -143,17 +143,18 @@ namespace AzureTest
                         if (rq <= 0)
                             break;
                         rec.coreused[ct - rq] = co;
-                        cpt.coreStatus[co] = 0;
+                        cpt.coreStatus[co] = 1;
                         rq = rq - 1;
                         cpt.used = cpt.used + 1;
                     }
                     reList. Add(rec);
                     restr = string.Format("{0} {1}", rec.compID.ToString(), rec.coreused[0].ToString());
+                    break;
                 }
             }
             else
             {
-                var rec2 = reList.Where(z => z.tID == rTemp.tID && z.reqID == rTemp.reqID).FirstOrDefault();
+                var rec2 = reList.Where(z => z.tID == rTemp.tID && z.reqID == rTemp.reqNum).FirstOrDefault();
                 if (rec2 != null)
                 {
                     var tgcomp = comList.Where(tg => tg.tID == rec2.tID && tg.compID == rec2.compID).FirstOrDefault();
@@ -178,16 +179,20 @@ namespace AzureTest
             return false;
         }
 
-        private static bool CheckM(int input)
+        private static bool CheckM(string input)
         {
-            if (input <= 0 || input > 10000)
+            int m = 0;
+            int.TryParse(input, out m);
+            if (m <= 0 || m > 10000)
                 return true;
             return false;
         }
 
-        private static bool CheckQ(int input)
+        private static bool CheckQ(string input)
         {
-            if (input <= 0 || input > 1000000)
+            int q = 0;
+            int.TryParse(input, out q);
+            if (q <= 0 || q > 1000000)
                 return true;
             return false;
         }
@@ -199,9 +204,21 @@ namespace AzureTest
             return false;
         }
 
-        private static bool CheckQtype(char input)
+        private static bool CheckQtype(string input)
         {
-            if (input != 'A' && input != 'F')
+            char c = '0';
+            char.TryParse(input, out c);
+            if (c != 'A' && c != 'F')
+                return true;
+            return false;
+        }
+
+        private static bool CheckReqNum(string input)
+        {
+            int r = 0
+                ;
+            int.TryParse(input, out r);
+            if (r < 0 || r > 128)
                 return true;
             return false;
         }
@@ -214,6 +231,8 @@ namespace AzureTest
         public int core;//核心数
         public int used;//已使用核心数
         public int[] coreStatus;//核心使用状况
+
+        public int rank;//优先级别
     }
 
     class Request
